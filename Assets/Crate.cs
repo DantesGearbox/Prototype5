0fb34a280e6c2a1eec7d1acbd24fe1115a8d437e
Frans Peter Larsen <larsen.frans@gmail.com>
2017-10-02 21:40:02 +0200

    Added crate with some basic crate functions

---
+++ Assets/Crate.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour
+{
+    public Texture2D[] crateTextures;
+    public CrateType type;
+
+    private MeshRenderer mesh;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mesh = GetComponent<MeshRenderer>();
+
+        int numOfTypes = System.Enum.GetNames(typeof(CrateType)).Length;
+        type = (CrateType)Random.Range(0, numOfTypes);
+
+        mesh.material.mainTexture = GetCrateTexture(type);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public Texture2D GetCrateTexture(CrateType type)
+    {
+        switch(type)
+        {
+            case CrateType.Type_1:
+                return crateTextures[0];
+            case CrateType.Type_2:
+                return crateTextures[1];
+            case CrateType.Type_3:
+                return crateTextures[2];
+        }
+
+        return null;
+    }
+}
+
+public enum CrateType
+{
+    Type_1,
+    Type_2,
+    Type_3
+}
