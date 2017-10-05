1bca78395ec886255d5ab25c699d9e9ad122e5af
Frans Peter Larsen <larsen.frans@gmail.com>
2017-10-05 11:26:53 +0200

    Man now runs around like a proper communistic comrade

---
+++ Assets/MovementAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAnim : MonoBehaviour
+{
+    Animator anims;
+    Rigidbody physics;
+
+	// Use this for initialization
+	void Start () {
+        physics = GetComponent<Rigidbody>();
+        anims = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        anims.SetFloat("Movement", physics.velocity.magnitude / 3);
+	}
+}
