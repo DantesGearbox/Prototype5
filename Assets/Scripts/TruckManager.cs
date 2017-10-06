8b84e5fa4a075dedf0bb10bd4763c65cd6185887
DantesGearbox <mads.engberg@hotmail.com>
2017-10-06 15:11:34 +0200

    Added the skeleton of a truck manager

---
+++ Assets/Scripts/TruckManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckManager : MonoBehaviour {
+
+	public List<TruckTiming> trucks;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+}
+
+public struct TruckTiming{
+
+	float arrival;
+	float departure;
+	GameObject obj;
+
+}
