90f6d795e4b1d95fedba2b79df2e67d58c46fef0
DantesGearbox <mads.engberg@hotmail.com>
2017-10-06 11:18:19 +0200

    GoalQuads are implemented

---
+++ Assets/Scripts/GoalQuad.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalQuad : MonoBehaviour {
+
+	public CrateType type;
+	public string pickUpObjectsTag = "PickUpObject";
+
+	void OnTriggerEnter(Collider other){
+		if(other.CompareTag (pickUpObjectsTag)){
+
+			Crate crate = other.GetComponent<Crate> ();
+
+			if (crate == null)
+				return;
+
+			if(crate.type == type){
+				//Add 100 points
+				GameManager.instance.AddScore (100);
+			} else {
+				//Subtract 50 points
+				GameManager.instance.AddScore (-50);
+			}
+
+			Debug.Log (GameManager.instance.GetScore ());
+
+			//Delete the crate
+			Destroy (crate.gameObject);
+		}
+	}
+}
