89e7bccac93b5e294d8dc17a78a10f2d0cee87e8
DantesGearbox <mads.engberg@hotmail.com>
2017-10-06 16:43:51 +0200

    Made picking up a crate toggleable, and made absolute player movement. These settings can be toggled on and off in the player object.

--- Assets/Scripts/Truck.cs
+++ Assets/Scripts/Truck.cs
@@ -24,12 +24,9 @@ public class Truck : MonoBehaviour {
 			GameObject obj = objectsInTruck [i];
 
 			if (obj.CompareTag ("Player")) {
-
+				//Teleport the player out
 				Vector3 newPosition = new Vector3 (transform.position.x, obj.transform.position.y, transform.position.z);
 				obj.transform.position = (-transform.right * 5) + newPosition;
-
-				//Teleport out
-
 
 			} else if (obj.CompareTag ("PickUpObject")) {
 				Destroy (obj);
@@ -45,8 +42,7 @@ public class Truck : MonoBehaviour {
 		//Spawn new crates
 		for(int i = 0; i < startingCratesAmount; i++){
 			//1.4 is a magic number that makes the crates stand on the floor
-			Vector3 pos = new Vector3 (Random.Range (bounds.min.x, bounds.max.x), bounds.center.y-1.4f, Random.Range (bounds.min.z, bounds.max.z)); 
-			Debug.Log (bounds.extents);
+			Vector3 pos = new Vector3 (Random.Range (bounds.min.x, bounds.max.x), bounds.center.y-1.4f, Random.Range (bounds.min.z, bounds.max.z));
 
 			Instantiate (cratePrefab, pos, Quaternion.identity);	
 		}
